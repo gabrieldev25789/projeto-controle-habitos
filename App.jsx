be4ce6599ab7d126545a065ed1022b4b796e8952
Gabriel <gabrielsb1090@gmail.com>
2026-05-25 06:57:11 -0300

    Definindo rotas

--- App.jsx
+++ App.jsx
@@ -1,29 +1,13 @@
-import { useState } from 'react'
-import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import Form from './Components/Form/Form'
+import Home from './Components/Home/Home'
 
 function App() {
-  const [user, setUser] = useState({})
-
   return (
-    <>
-      <h2>Bem vindo</h2>
-
-        <form>
-          <label>Nome de usuário</label>
-          <input type="text" placeholder="seu_usuario" />
-
-          <label>E-mail</label>
-          <input type="email" placeholder="voce@email.com" />
-
-          <label>Senha</label>
-          <input type="password" placeholder="••••••••" />
-
-          <label>Confirmar senha</label>
-          <input type="password" placeholder="••••••••" />
-
-          <button type="submit">Criar conta</button>
-        </form>
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/cadastro" element={<Form />} />
+    </Routes>
   )
 }
 
