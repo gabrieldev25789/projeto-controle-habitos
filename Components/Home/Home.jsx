926147e83f1b7c8635936cb022a02937d011d880
Gabriel <gabrielsb1090@gmail.com>
2026-05-27 06:03:31 -0300

    feat: Passando prop pro body e criando estado para fechar modal

--- Components/Home/Home.jsx
+++ Components/Home/Home.jsx
@@ -1,12 +1,16 @@
+// Home.jsx
+import { useState } from "react"
 import Header from "../Header/Header"
 import Body from "../Body/Body"
 
 function Home() {
+  const [showModal, setShowModal] = useState(true)
+
   return (
     <>
-        <Header />
-        <Body />
-        <div>Home</div>
+      <Header />
+      {showModal && <Body onFinish={() => setShowModal(false)} />}
+      <div>Home</div>
     </>
   )
 }
