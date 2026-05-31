4b52a196c98dca92e029ca1a90c3bad9208d03ae
Gabriel <gabrielsb1090@gmail.com>
2026-05-31 05:21:38 -0300

    feat: Criando estados e passando props

--- App.jsx
+++ App.jsx
@@ -3,16 +3,33 @@ import { useState } from 'react';
 import Form from './Components/Form/Form'
 import Home from './Components/Home/Home'
 
+
 function App() {
-    const [user, setUser] = useState(() => {
-      const saved = localStorage.getItem("user")
-      return saved ? JSON.parse(saved) : {}
-    })
+  const [user, setUser] = useState(() => {
+  const emailAtivo = localStorage.getItem("sessao")
+  if (!emailAtivo) return {}
+    const saved = localStorage.getItem(`user-${emailAtivo}`)
+    return saved ? JSON.parse(saved) : {}
+  })
+
+  const [dadosUser, setDadosUser] = useState(() => {
+    const emailAtivo = localStorage.getItem("sessao")
+    if (!emailAtivo) return {}
+    const dados = localStorage.getItem(`dadosUser-${emailAtivo}`)
+    return dados ? JSON.parse(dados) : {}
+  })
+
+  function handleSetUser(novoUser) {
+    localStorage.setItem("sessao", novoUser.email) // salva sessão ativa
+    setUser(novoUser)
+    const dados = localStorage.getItem(`dadosUser-${novoUser.email}`)
+    setDadosUser(dados ? JSON.parse(dados) : {})
+  }
 
   return (
     <Routes>
-      <Route path="/" element={<Form usuarioSalvo={user} setUser={setUser} />} />
-      <Route path="/cadastro" element={<Home usuarioSalvo={user} />} />
+      <Route path="/" element={<Form setUser={handleSetUser} />} />
+      <Route path="/cadastro" element={<Home usuarioSalvo={user} dadosUser={dadosUser} setDadosUser={setDadosUser} />} />
     </Routes>
   )
 }
