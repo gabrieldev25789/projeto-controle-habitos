55d9718145721dae62c759448334d5e7bc4e00b6
Gabriel <gabrielsb1090@gmail.com>
2026-06-06 05:31:41 -0300

    feat: Inicializando dados e passando props

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,25 @@ import "./App.css"
 import { Routes, Route } from "react-router-dom"
 import  Form  from "./Components/Form/Form.jsx"
 import Home from "./Components/Home/Home.jsx"
+import { useState } from "react"
 
 
 function App() {
+
+    const [dadoUser, setDadoUser] = useState(()=>{
+    const emailAtivo = localStorage.getItem("sessao")
+    if(!emailAtivo) return null 
+    return JSON.parse(localStorage.getItem(`user-${emailAtivo}`))
+  })
+
+  const [email, setEmail] = useState(() => {
+    return localStorage.getItem("sessao") || ""
+  })
+
    return (
     <Routes>
-      <Route path="/" element={<Form />} />
-      <Route path="/Home" element={<Home />} />
+      <Route path="/" element={<Form setDadoUser={setDadoUser} email={email} setEmail={setEmail}/>} />
+      <Route path="/Home" element={<Home dadoUser={dadoUser} email={email}/>} />
     </Routes>
   )
 }
