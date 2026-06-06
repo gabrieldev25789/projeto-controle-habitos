22c1f2177e02a0456f5fb6f5669639d7f00b87a3
Gabriel <gabrielsb1090@gmail.com>
2026-06-06 04:45:12 -0300

    feat: Usando rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import "./App.css"
+import { Routes, Route } from "react-router-dom"
 import  Form  from "./Components/Form/Form.jsx"
+import Home from "./Components/Home/Home.jsx"
 
 
 function App() {
-  return (
-    <Form />
+   return (
+    <Routes>
+      <Route path="/" element={<Form />} />
+      <Route path="/Home" element={<Home />} />
+    </Routes>
   )
 }
 
