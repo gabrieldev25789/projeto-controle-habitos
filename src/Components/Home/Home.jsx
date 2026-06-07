0e239e71c1db144846a671c154a5968be0d5e372
Gabriel <gabrielsb1090@gmail.com>
2026-06-06 23:23:39 -0300

    feat: Criando estado de modal aberto e renderizando modal

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,15 +1,28 @@
+import { useState } from "react"
 import Header from "../Header/Header.jsx"
+import Modal from "../Modal/Modal.jsx"
 
 function Home({ email }) {
+
+  const [modalAberto, setModalAberto] = useState(false)
 
   const salvo = JSON.parse(localStorage.getItem(`user-${email}`))
 
   if(!salvo) return <p>Carregando...</p>
 
+  function abreModal(){
+    setModalAberto(true)
+  }
+
   return (
     <>
       <Header />
-      <h2>Bem vindo {salvo.nome}</h2>
+      {!modalAberto && 
+      <h2>Bem vindo {salvo.nome}</h2>}
+
+      <button onClick={() => abreModal()}>AAAAAAAA</button>
+
+      {modalAberto && <Modal />}
     </>
   )
 
